23955b1253ff8069bbd64542f62f4f88f20d7f1c
yuwenhao <1471689575@qq.com>
2020-04-23 17:20:45 +0800

    开荤

---
+++ springboot/src/test/java/priv/wenhao/securitytest/SecuritytestApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.wenhao.securitytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
